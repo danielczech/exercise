7cc82d504493e6c8e2ab64d629dc37cea1cff586
danielczech <daniel.czech@protonmail.com>
2020-01-20 14:32:26 +0200

    Handle toggle switches in JS.

--- js/memsettings.js
+++ js/memsettings.js
@@ -17,12 +17,33 @@ function randomString(len, alphanum){
   return items;
 }
 
+function setAlphaNum(check){
+  state = check.checked;
+  localStorage.setItem("alphaNum", JSON.stringify(state));
+}
+
+function setReversed(check){
+  state = check.checked;
+  localStorage.setItem("reversed", JSON.stringify(state));
+}
+
 function defaults(){
   // Default settings on load.
+  // Number of characters:
   defaultLen = 8;
   localStorage.setItem("len", JSON.stringify(defaultLen));
   showLen = document.getElementById("showLen");
   showLen.innerHTML = defaultLen;
+  // Alphanumeric? 
+  alphaNumState = false;
+  localStorage.setItem("alphaNum", JSON.stringify(alphaNumState));
+  alphaNumCheck = document.getElementById("alphaNumCheck");
+  alphaNumCheck.checked = false;
+  // Reversed?
+  reversedState = false;
+  localStorage.setItem("alphaNum", JSON.stringify(reversedState));
+  reversedCheck = document.getElementById("reversedCheck");
+  reversedCheck.checked = false;
 }
 
 function changeLen(sign){
