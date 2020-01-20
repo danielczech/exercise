3394ef38582576a14b1c13869181c5197800af4f
danielczech <daniel.czech@protonmail.com>
2020-01-20 19:31:50 +0200

    Form entry.

--- js/memtest.js
+++ js/memtest.js
@@ -8,8 +8,33 @@ function displayTimed(delay){
   // by delay.
   testSequence = JSON.parse(localStorage.getItem("testSequence"));
   testLen = JSON.parse(localStorage.getItem("testSequence"));
-
   dispSequence = document.getElementById("testSequence");
-  dispSequence.innerHTML = testSequence;
-
+  dispSequence.innerHTML = testSequence.join("");
+  setTimeout(clearSequence, delay);
+  setTimeout(dispForm, delay);
 }
+
+function checkAns(){
+	ansInput = document.getElementById("ansInput");
+	alert(ansInput.value);
+}
+
+function clearSequence(){
+  dispSequence = document.getElementById("seqDiv");
+  dispSequence.style.display = "none"; 
+}
+
+function dispForm(){
+  dispForm = document.getElementById("ansForm");
+  dispForm.style.display = "block"; 
+  check = document.getElementById("check");
+  check.style.display = "block"; 
+  ansInput= document.getElementById("ansInput");
+  ansInput.focus();
+  ansInput.select();
+}
+
+
+
+
+
