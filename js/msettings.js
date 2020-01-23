30d83e4bb08d32ca294cca63ecf4f1a6a342f8ca
danielczech <daniel.czech@protonmail.com>
2020-01-23 22:34:21 +0200

    Initial settings page complete.

---
+++ js/msettings.js
@@ -0,0 +1,52 @@
+// MIT licence (see licence.txt). D Czech 2020.
+// Settings for arithmetic problems.
+
+function defaults(){
+  // Default settings on load.
+  // N digits A:
+  defaultA = 2;
+  localStorage.setItem("A", JSON.stringify(defaultA));
+  showA = document.getElementById("showA");
+  showA.innerHTML = lenDigits(defaultA);
+  // N digits B:
+  defaultB = 2;
+  localStorage.setItem("B", JSON.stringify(defaultB));
+  showB = document.getElementById("showB");
+  showB.innerHTML = lenDigits(defaultB);
+  // N items for add/subtract
+  defaultN = 2;
+  localStorage.setItem("N", JSON.stringify(defaultN));
+  showN = document.getElementById("showN");
+  showN.innerHTML = lenDigits(defaultN);
+}
+
+function runTest(){
+  window.location.href = "mtest.html"
+}
+
+function lenDigits(output){
+  // Ensure that output number has spaces
+  // prepended to it for consistency
+  if(output < 10){
+    outputString = "0" + output.toString();
+  }
+  else{
+    outputString = output.toString();
+  }
+  return outputString;
+}
+
+function change(sign, name){
+  // Increment or decrement length.
+  // Min length 3 characters.
+  len = JSON.parse(localStorage.getItem(name));
+  if(sign == "+"){
+    len = len + 1;
+  }
+  else if(sign == "-" && len > 2) {
+    len = len - 1;
+  }
+  localStorage.setItem(name, JSON.stringify(len));
+  showLen = document.getElementById("show" + name);
+  showLen.innerHTML = lenDigits(len);
+}
